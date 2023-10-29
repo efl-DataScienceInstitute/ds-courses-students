605d82b0df51f4806a6bbb79744364fe1c93ffcb
BenjaminMAK <BenjaminMAK@users.noreply.github.com>
2023-10-29 16:32:38 +0100

    Create main.py
    main file to play

---
+++ course material/1-Introduction-to-Python/day-1/1_introduction/main.py
@@ -0,0 +1,9 @@
+# -------
+# Preamble and Semantic Versioning
+# -------
+# @author: Your Name
+# @since: yyyy-mm-dd 
+# @update: yyyy-mm-dd 
+# @version: <major><minor><update>
+# @workload X min
+print('Hello World')
