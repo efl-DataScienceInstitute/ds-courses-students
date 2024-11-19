e20846ef9eec98d061f290dda5e58376dfcea163
TCestonaro <53000079+TCestonaro@users.noreply.github.com>
2024-11-19 12:53:53 +0100

    Course 2024

--- course material/1-Introduction-to-Python/day-1/1_introduction/main.py
+++
@@ -1,9 +0,0 @@
-# -------
-# Preamble and Semantic Versioning
-# -------
-# @author: Your Name
-# @since: yyyy-mm-dd 
-# @update: yyyy-mm-dd 
-# @version: <major><minor><update>
-# @workload X min
-print('Hello World')
